230ea2c0916a3769d7db6c3d5efb83dc2d3da98f
Garvita Gupta <167073450+thalaforever@users.noreply.github.com>
2025-11-19 00:08:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+let  boxess = document.querySelectorAll(".boxes");
+let btn = document.querySelector("#rest");
+let turn0 = true ;
+const arr= [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+];
+boxess.forEach((box) => {
+    box.addEventListener("click" , () => {
+    console.log("clicked button");
+})
+})
